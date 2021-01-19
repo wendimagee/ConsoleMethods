ec975216185f151d943fd36e88e290444296c283
Wendi Magee <wendimagee@gmail.com>
2021-01-19 13:30:54 -0500

    initial commit

---
+++ ConsoleMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
